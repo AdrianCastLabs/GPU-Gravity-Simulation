a349db26b6c3b1e13af5b0f896e8650f454dd638
Adrian Castillo <adrian.cast@icloud.com>
2026-08-20 01:29:59 +0200

    Added simple gravity

--- GPU-Gravity-Simulation/Assets/Scripts/SimulationManager.cs
+++ GPU-Gravity-Simulation/Assets/Scripts/SimulationManager.cs
@@ -4,12 +4,40 @@ using Random = UnityEngine.Random;
 
 public class SimulationManager : MonoBehaviour
 {
+    // Simulation settings
     public GameObject particlePrefab;
-
+    
+    // Simulation parameters
+    public int nParticles;
     public float simulationSize;
+    public float gravityMultiplier;
+    
+    private Vector3[] positions;
+    private GameObject[] gameObjects;
+
     
     private void Start()
     {
-        GameObject newParticle = Instantiate(particlePrefab, Random.insideUnitCircle * simulationSize, Quaternion.identity);
+        // Initialize arrays
+        positions = new Vector3[nParticles];
+        gameObjects = new GameObject[nParticles];
+
+        // Initialize particles
+        for (int i = 0; i < nParticles; i++)
+        {
+            positions[i] = Random.insideUnitSphere * simulationSize;
+            gameObjects[i] = Instantiate(particlePrefab, positions[i], Quaternion.identity);
+        }
+        
+    }
+
+    private void Update()
+    {
+        // Simulation loop
+        for (int i = 0; i < nParticles; i++)
+        {
+            positions[i].y -= 1f * Time.deltaTime * gravityMultiplier;
+            gameObjects[i].transform.position = positions[i];
+        }
     }
 }
